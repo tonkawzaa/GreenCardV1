186b8745eae96c965ea516e2c2d0fd9ad103fbe3
piyawat saikun <tonkaw.zaa@gmail.com>
2016-03-14 05:38:34 +0000

    Changes made by piyawat saikun (tonkaw.zaa@gmail.com).

--- GreenCard/components/home/index.js
+++ GreenCard/components/home/index.js
@@ -2,9 +2,17 @@
 
 app.home = kendo.observable({
     onShow: function() {},
-    afterShow: function() {}
+    afterShow: function() {},
+    login: function() {
+        			navigator.notification.alert("login");
+  
+    },
+     register: function() {
+        			navigator.notification.alert("register");
+  
+    },
 });
-
+kendo.bind($('#submitfunc'),app.home);
 // START_CUSTOM_CODE_home
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
