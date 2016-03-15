7404e9f89de7240439739622de48bdef197eaf11
piyawat saikun <tonkaw.zaa@gmail.com>
2016-03-15 04:41:51 +0000

    Changes made by piyawat saikun (tonkaw.zaa@gmail.com).

---
+++ GreenCard/components/welcome/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.welcome = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_welcome
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_welcome
